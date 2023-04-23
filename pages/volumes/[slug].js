edfac71bdbe375b2a5c81dd1db3d70eb4bd87bde
Maria del Mar <chocoinformatics2016@gmail.com>
2023-04-23 08:41:45 +0200

    Finished with dynamic routing challenge

---
+++ pages/volumes/[slug].js
@@ -0,0 +1,41 @@
+import { volumes } from "../../lib/data";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+export default function Volume() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const volume = volumes.find((volumeToFind) => volumeToFind.slug === slug);
+  const volumeIndex = volumes.findIndex(
+    (volumeToFind) => volumeToFind.slug === slug
+  );
+  const nextVolumeIndex = volumeIndex < 2 ? volumeIndex + 1 : 0;
+
+  if (!volume) {
+    return null;
+  }
+
+  const { title, description, books, cover } = volume;
+  return (
+    <>
+      <div>
+        <Link href="/">All Volumes</Link>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <ul>
+          {books.map((book, id) => (
+            <li key={id}>
+              {book.ordinal}-{book.title}
+            </li>
+          ))}
+        </ul>
+        <Image src={cover} alt="image" height={230} width={140} />
+
+        <Link href={`/volumes/${volumes[nextVolumeIndex].slug}`}>
+          Next Volume : {volumes[nextVolumeIndex].title}
+        </Link>
+      </div>
+    </>
+  );
+}
